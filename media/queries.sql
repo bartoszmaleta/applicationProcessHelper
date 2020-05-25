60a019e4a4ac95b469d08ee27de4e2b89c13175d
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-05-25 03:40:05 +0200

    Add query 4

--- media/queries.sql
+++ media/queries.sql
@@ -1,7 +1,12 @@
 SELECT first_name, last_name FROM mentors;
 
--- SELECT nick_name FROM mentors WHERE city='Miskolc';
+SELECT nick_name FROM mentors WHERE city='Miskolc';
 
 SELECT CONCAT(first_name, ' ', last_name) AS "Full name", phone_number
 FROM mentors
 WHERE first_name='Carol';
+
+SELECT CONCAT(first_name, ' ', last_name) AS "Full name", phone_number
+FROM mentors
+WHERE email LIKE '%@adipicingenimmi.edu';
+
